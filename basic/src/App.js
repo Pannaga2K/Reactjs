41915b0e175e5c42a8407f0242da73a892841fba
Pannaga2K <50201854+Pannaga2K@users.noreply.github.com>
2020-07-25 12:48:21 +0530

    Add files via upload

---
+++ basic/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Switch, Link, Route} from "react-router-dom";
+import Info from "./E1/Info";
+import Num from "./E1/Num";
+import Prime from "./E1/Prime";
+import Todo from "./E1/Todo";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/info">
+            <Info />
+            <Link to="/todo">TODO</Link>
+            <Link to="/num">NUM</Link><br/>
+            <Link to="/prime">PRIME</Link>
+          </Route>
+          <Route path="/num">
+            <Num />
+            <Link to="/info">INFO</Link><br/>
+            <Link to="/prime">PRIME</Link>
+            <Link to="/todo">TODO</Link>
+          </Route>
+          <Route path="/prime">
+              <Prime />
+              <Link to="/info">INFO</Link><br/>
+              <Link to="/todo">TODO</Link>
+              <Link to="/num">NUM</Link>
+          </Route>
+          <Route path="/todo">
+              <Todo />
+              <Link to="/prime">PRIME</Link><br/>
+              <Link to="/info">INFO</Link><br/>
+              <Link to="/num">NUM</Link>
+          </Route>
+          <Route path="/">
+            <Link to="/info">INFO</Link><br/>
+            <Link to="/num">NUM</Link><br/>
+            <Link to="/prime">PRIME</Link><br/>
+            <Link to="/todo">TODO</Link>
+          </Route>
+        </Switch>  
+      </div>
+    </Router>
+  );
+}
+
+export default App;
